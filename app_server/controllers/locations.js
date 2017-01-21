0f51a7ccaec3717b19667504986a0ad813f18d98
RichardRuiz <john@doe.com>
2017-01-20 22:26:56 -0800

    basic layout w/ dummy info added

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,9 +3,9 @@ module.exports.homelist = function(req, res){
 };
 
 module.exports.locationInfo = function(req, res){
-	res.render('index', {title : "Location Info"});
+	res.render('location-info', {title : "Location Info"});
 };
 
 module.exports.addReview = function(req, res){
-	res.render('index', {title : "Add Review"});
+	res.render('location-review-form', {title : "Add Review"});
 };
