18f5876c4a782021b4a37d46760aee48c8b1d269
maxwell cudlitz <maxwellcudlitz@loki.pugetsound.edu>
2017-02-22 20:40:46 -0800

    geoNear in the api locations implemented. Need to do error checking

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,43 +1,65 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJsonResponse = function(res, status, content){
+var sendJsonResponse = function (res, status, content) {
     res.status(status);
     res.json(content);
 };
 
-var theEarth = (function(){
+var theEarth = (function () {
 
     var earthRadius = 6371;
 
-    var getDistanceFromRads = function(rads){
+    var getDistanceFromRads = function (rads) {
         return parseFloat(rads * earthRadius);
     };
 
-    var getRadsFromDistance = function(distance){
+    var getRadsFromDistance = function (distance) {
         return parseFloat(distance / earthRadius);
     };
 
-    return{
-        getDistanceFromRads : getDistanceFromRads,
-        getRadsFromDistance : getRadsFromDistance
+    return {
+        getDistanceFromRads: getDistanceFromRads,
+        getRadsFromDistance: getRadsFromDistance
     };
 });
 
+// get list of locations
+module.exports.locationsListByDistance = function (req, res) {
+
+    // using the lat+lng data and the theEarth function, determines the 
+    // distance
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var maxDistance = parseFloat(req.query.maxDistance);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(maxDistance),
+        num: 10
+    };
+
+    // passes the geoOptions to the geoNear function
+    Loc.geoNear(point, geoOptions, callback);
+};
+
 // reads one location from the database.
-module.exports.locationsReadOne = function(req, res) {
-    
-    if(req.params && req.params.locationid){
+module.exports.locationsReadOne = function (req, res) {
+
+    if (req.params && req.params.locationid) {
         // req.params.locationid; locationid field of URL resolved as a string (id),
         // and passed to database.
         Loc.findById(req.params.locationid)
             // Executes the query, defined by an anonymous function that takes an err callback +
             // the location object, defined by exec( err: any, res: Document)
-            .exec(function(err, location){
-                if(!location){
-                    sendJsonResponse(res, 404, {"message" : "locationid not found"});
+            .exec(function (err, location) {
+                if (!location) {
+                    sendJsonResponse(res, 404, { "message": "locationid not found" });
                     return;
-                } else if (err){
+                } else if (err) {
                     sendJsonResponse(res, 404, err);
                     return;
                 }
@@ -46,23 +68,23 @@ module.exports.locationsReadOne = function(req, res) {
             });
     } else {
         // if no locationid was present (no req.params OR requ.params.locationid)
-        sendJsonResponse(res, 404, {"message" : "No locationid in request"});
+        sendJsonResponse(res, 404, { "message": "No locationid in request" });
     }
-    
+
 
 };
 
-module.exports.locationsCreate = function(req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+module.exports.locationsCreate = function (req, res) {
+    sendJsonResponse(res, 200, { "status": "success" });
 };
 
-module.exports.locationsListByDistance = function(req, res) {
-    sendJsonResponse(res, 201, {"status" : "success"});
+module.exports.locationsListByDistance = function (req, res) {
+    sendJsonResponse(res, 201, { "status": "success" });
 };
 
-module.exports.locationsUpdateOne = function(req, res){
-    sendJsonResponse(res, 201), {"status" : "success"};
+module.exports.locationsUpdateOne = function (req, res) {
+    sendJsonResponse(res, 201), { "status": "success" };
 };
-module.exports.locationsDeleteOne = function(req, res){
-    sendJsonResponse(res, 204), {"status" : "success"};
+module.exports.locationsDeleteOne = function (req, res) {
+    sendJsonResponse(res, 204), { "status": "success" };
 };
