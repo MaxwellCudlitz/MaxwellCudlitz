3a95abbe7d16cefe1c7c2e07f7777b70d76df12e
Maxwell Cudlitz <mcudlitz@pugetsound.edu>
2017-03-09 21:19:03 -0800

    added creation of locations to locations.js

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -24,7 +24,8 @@ var theEarth = (function () {
     };
 });
 
-// get list of locations
+// get list of locations, sorted by distance. Catches errors.
+// returns JSON responses.
 module.exports.locationsListByDistance = function (req, res) {
 
     // using the lat+lng data and the theEarth function, determines the 
@@ -59,16 +60,51 @@ module.exports.locationsListByDistance = function (req, res) {
         // else iterate over results in doc && respond
         else {
             results.forEach(function(doc) {
-             locations.push({
-             distance: theEarth.getDistanceFromRads(doc.dis),
-             name: doc.obj.name,
-             address: doc.obj.address,
-             rating: doc.obj.rating,
-             facilities: doc.obj.facilities,
-             _id: doc.obj._id
-             });
+                locations.push({
+                distance: theEarth.getDistanceFromRads(doc.dis),
+                name: doc.obj.name,
+                address: doc.obj.address,
+                rating: doc.obj.rating,
+                facilities: doc.obj.facilities,
+                _id: doc.obj._id
+                });
          });
          sendJsonResponse(res, 200, locations);
+        }
+    });
+};
+
+// creates a location, stores it in the database as a document.
+// Accessible through the REST Api.
+module.exports.locationsCreate = function(req, res) {
+
+    // creates the location object using the schema
+    // - schema additionally ensures proper formatting
+    // and handles malformed request security.
+    // TODO: Check on mongodb rules for strict document formation(?) Could be an attack vector for DOS with large BLOB file
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+        }]
+    }, 
+    // callback function that informs client of success/failure
+    function(err, location) {
+        if (err) {
+        sendJsonResponse(res, 400, err);
+        } else {
+        sendJsonResponse(res, 201, location);
         }
     });
 };
