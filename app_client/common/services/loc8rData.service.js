51ce62e81ec369d862f6712263a96c76f12a055f
Maxwell Cudlitz <mcudlitz@pugetsound.edu>
2017-03-14 02:16:37 -0700

    fixed everything but the map

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -10,13 +10,18 @@
       return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=2000000000000');
     };
 
+    var addReviewById = function (locationid, data) {
+      return $http.post('/api/locations/' + locationid + '/reviews', data);
+    };
+
     var locationById = function (locationid) {
         return $http.get('/api/locations/' + locationid);
       };
 
       return {
         locationByCoords : locationByCoords,
-        locationById : locationById
+        locationById : locationById,
+        addReviewById : addReviewById
       };
   }
 
