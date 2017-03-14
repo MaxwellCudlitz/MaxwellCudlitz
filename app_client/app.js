aed61df79253aebab80f03d7dc1a077f961bd49c
Maxwell Cudlitz <mcudlitz@pugetsound.edu>
2017-03-13 20:11:55 -0700

    user routing in progress; added local template + routing libraries.

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,19 @@
 angular.module('loc8rApp', []);
+
+// is actualy angular routing
+angular.module('loc8rApp', ['ngRoute']);
+
+// configures routing for SPA
+function config ($routeProvider) {
+	$routeProvider
+	.when('/', {
+
+		// point to new template
+		templateUrl: 'home/home.view.html'
+	})
+	.otherwise({redirectTo: '/'});
+}
+
+angular
+.module('loc8rApp')
+.config(['$routeProvider', config]);
