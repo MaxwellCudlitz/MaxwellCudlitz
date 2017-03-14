c4289c5da6ab249a575e41fabb5a4d1d65c2069f
Maxwell Cudlitz <mcudlitz@pugetsound.edu>
2017-03-13 23:46:34 -0700

    hotfix

--- app_client/app.js
+++ app_client/app.js
@@ -2,11 +2,21 @@
 
   angular.module('loc8rApp', ['ngRoute']);
 
+  // little bug fix for stupid user scenarios
+  if (window.location.pathname !== '/') {
+    window.location.href = '/#' + window.location.pathname;
+  } 
+
   function config ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'home/home.view.html',
         controller: 'homeCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/about', {
+        templateUrl: '/common/views/genericText.view.html',
+        controller: 'aboutCtrl',
         controllerAs: 'vm'
       })
       .otherwise({redirectTo: '/'});
@@ -16,6 +26,6 @@
 
   angular
     .module('loc8rApp')
-    .config(['$routeProvider', config]);
+    .config(['$routeProvider','$locationProvider', config]);
 
 })();
